bbdff09a14be19e6d692043077b554323e5ca958
Herman Chau <beijingherman@gmail.com>
2024-11-08 10:03:41 -0800

    Fix section 1.2 theorem definition

--- GeneratingFunctions/Section1.2.lean
+++ GeneratingFunctions/Section1.2.lean
@@ -7,6 +7,6 @@ open PowerSeries
 noncomputable def Y : ℝ⟦X⟧ := PowerSeries.X
 
 theorem partial_fraction_expansion (A B C:ℝ⟦X⟧):
-    (1-(2:ℝ)•Y+(2:ℝ)•Y*Y) * (1-Y)⁻¹ * (1-Y)⁻¹ * (1-(2:ℝ)•Y)⁻¹ (1-(2:ℝ)•Y)⁻¹
+    (1-(2:ℝ)•Y+(2:ℝ)•Y*Y) * (1-Y)⁻¹ * (1-Y)⁻¹ * (1-(2:ℝ)•Y)⁻¹ * (1-(2:ℝ)•Y)⁻¹
     = A * (1-Y)⁻¹ * (1-Y)⁻¹ + B * (1-Y)⁻¹ + C * (1-(2:ℝ)•Y)⁻¹ := by
   sorry
